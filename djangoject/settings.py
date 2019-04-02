731735bf77e5535fc50162710e78b4bce6534e24
Grokoko <ras.algathi@gmail.com>
2019-04-02 23:15:33 +0200

    trying sth

--- djangoject/settings.py
+++ djangoject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rp^529&1%^s-8t*9jty*^t#$y164j3$x*q2j%dm!1f5m1(xr-6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS=['fierce-brook-69212.herokuapp.com',]
 
 
 # Application definition
