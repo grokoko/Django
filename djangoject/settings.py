e195ac30635b67e9ddd2e7f16c3eaae7927dc126
Grokoko <ras.algathi@gmail.com>
2019-04-02 23:23:52 +0200

    trying sth

--- djangoject/settings.py
+++ djangoject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rp^529&1%^s-8t*9jty*^t#$y164j3$x*q2j%dm!1f5m1(xr-6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS=['fierce-brook-69212.herokuapp.com',]
 
