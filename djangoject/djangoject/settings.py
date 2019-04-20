7c734cebc566284db459b8b2c6173b0743b1b9a8
Grokoko <ras.algathi@gmail.com>
2019-04-20 12:03:40 +0200

    repairing db

--- djangoject/djangoject/settings.py
+++ djangoject/djangoject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'grokoko_db',
+        'USER': 'grokoko',
+        'PASSWORD': 'Jesien2013!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
