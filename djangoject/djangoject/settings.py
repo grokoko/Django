e0bbb3f6a99c69692f7c527df49046d00e92aa54
Grokoko <ras.algathi@gmail.com>
2019-04-21 14:17:23 +0200

    db

--- djangoject/djangoject/settings.py
+++ djangoject/djangoject/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'djangoject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'grokoko_db',
-        'USER': 'postgres',
-        'PASSWORD': 'test',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
